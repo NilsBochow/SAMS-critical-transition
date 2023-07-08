8e0ac776d87125d83a9e146083658b95c5f89694
Nils <nils.bochow@uit.no>
2023-07-08 11:11:01 +0200

    Include example for dry season calculations

--- Analysis/dry_season_paper.py
+++ Analysis/dry_season_paper.py
@@ -5,14 +5,18 @@ from scipy import stats
 from skimage.morphology import binary_opening
 from scipy.ndimage import uniform_filter1d
 import netCDF4 as nc4
-
+from matplotlib import pyplot as plt
+import os
 ### anomaly, pentad sum, 30d climatological 
 """ 
 For this script, there are some files needed: 
 precip_all_years_lat_lon : A file with spatial daily precipitation for all years. For the paper, we use the freely available ERA5 precipitation.
 climatological_mean: A file with the climatological mean of the precipitation or can be calculated from the precip_all_years_lat_lon variable
 
-These functions can be used (with slight modifications) for one site/area (Fig. 5B) or for spatial analysis as shown in the SI. 
+These functions can be used (with slight modifications) for one site/area (Fig. 5B) or for spatial analysis as shown in the SI. '
+
+At the end of the script, there is an example for 2 years of precipitation (2008, 2009) taken from ERA5. 
+Source: Hersbach, H., Bell, B., Berrisford, P., Biavati, G., Horányi, A., Muñoz Sabater, J., Nicolas, J., Peubey, C., Radu, R., Rozum, I., Schepers, D., Simmons, A., Soci, C., Dee, D., Thépaut, J-N. (2023): ERA5 hourly data on single levels from 1940 to present. Copernicus Climate Change Service (C3S) Climate Data Store (CDS)
 """
 
 
@@ -20,6 +24,8 @@ These functions can be used (with slight modifications) for one site/area (Fig. 
 """
 Some short functions used for calculation. A variable precip_nc4 is needed to infer the latitude/longitude and time variable.  
 """
+directory = dir_path = os.path.dirname(os.path.realpath(__file__)) + "/daily_precip/"
+precip_nc4 = nc4.Dataset(directory + "2009.nc")
 
 def load_lat_lon(): 
     lat = precip_nc4.variables['lat'][:]
@@ -33,8 +39,8 @@ def load_tlen():
     time =  precip_nc4.variables["time"][:]
     return time.shape[0]
 
-def lin_reg(DSL_yearly): 
-    slope, intercept, rvalue, pvalue, stderr = stats.linregress(np.arange(41), DSL_yearly) 
+def lin_reg(DSL_yearly, years): 
+    slope, intercept, rvalue, pvalue, stderr = stats.linregress(np.arange(years), DSL_yearly) 
     return slope, intercept
 
 def rolling_mean_3D(window, array, axis): 
@@ -244,19 +250,19 @@ def calculate_DSL_30d(year):
 
     return WSL
 
-def calculate_all_methods(): 
+def calculate_all_methods(years): 
     """ 
     Store results of all methods in one array.
     """
-    DSL_lat_lon = np.zeros((3, 41))
-    DSL_lat_lon_onset = np.zeros((3, 41))
-    WSL_lat_lon_onset = np.zeros((3, 41))
+    DSL_lat_lon = np.zeros((3, years))
+    DSL_lat_lon_onset = np.zeros((3, years))
+    WSL_lat_lon_onset = np.zeros((3, years))
 
     slope = np.zeros((3, 2))
     slope_onset_DSL = np.zeros((3, 2))
     slope_onset_WSL = np.zeros((3, 2))
 
-    for year in range(41):
+    for year in range(years):
         WSL = calculate_DSL_30d(year)
         DSL_lat_lon[0, year], DSL_lat_lon_onset[0, year], WSL_lat_lon_onset[0, year] = np.apply_along_axis(longestZeroSeqLength, 0, WSL)
         
@@ -265,10 +271,56 @@ def calculate_all_methods():
         DSL_lat_lon[2, year], DSL_lat_lon_onset[2, year], WSL_lat_lon_onset[2, year] = calculate_DSL_pentad(year)
 
     for i in range(3): 
-        slope[i, :] = lin_reg(DSL_lat_lon[i, :]) 
-        slope_onset_DSL[i, :] = lin_reg(DSL_lat_lon_onset[i, :])    
-        slope_onset_WSL[i, :] = lin_reg(WSL_lat_lon_onset[i, :])    
+        slope[i, :] = lin_reg(DSL_lat_lon[i, :], years) 
+        slope_onset_DSL[i, :] = lin_reg(DSL_lat_lon_onset[i, :], years)    
+        slope_onset_WSL[i, :] = lin_reg(WSL_lat_lon_onset[i, :], years)    
 
     return WSL, DSL_lat_lon, DSL_lat_lon_onset, WSL_lat_lon_onset, slope, slope_onset_DSL, slope_onset_WSL
 
-WSL, DSL, DSL_onset, WSL_onset, slope, slope_onset_DSL, slope_onset_WSL =  calculate_all_methods()
+
+
+# Example for 2 years of precipitation
+data1  = nc4.Dataset(directory + "2008.nc")["tprate"][:]
+data2 =  nc4.Dataset(directory + "2009.nc")["tprate"][:]
+precip_all_years_lat_lon = np.zeros(2, dtype = 'object')
+precip_all_years_lat_lon[0] = data1
+precip_all_years_lat_lon[1] = data2
+
+climatological_mean = np.nanmean(np.concatenate(precip_all_years_lat_lon), axis = 0).filled(np.nan)
+
+WSL, DSL, DSL_onset, WSL_onset, slope, slope_onset_DSL, slope_onset_WSL =  calculate_all_methods(2)
+
+
+"""
+Plotting the resulting DSL for the two years. 
+Note that the values are different from the ones shown in the paper because in this example here, we define the climatological mean as the average of only 2 years. 
+Method 1 returns the DSL in days while the other two methods return it in pentads.
+"""
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
+
+ax1.plot(DSL[0]/5, lw = 2, label = "Method 1")
+ax1.plot(DSL[1], lw = 2, label= "Method 2")
+ax1.plot(DSL[2], lw = 2, label = "Method 3")
+ax1.legend()
+ax1.set_xticklabels(["2008", "2009"])
+ax1.set_xticks([0, 1])
+ax1.set_xlabel("Year AD")
+ax1.set_ylabel("DSL [pentads]")
+ax1.set_title("Example DSL in pentads")
+
+ax2.set_title("Daily Precip 2008 in South AMZ")
+ax2.plot(mean_area_precip(precip_all_years_lat_lon[0], lat_min=lat_min, lat_max=lat_max, lon_min=lon_min, lon_max=lon_max), lw = 2, color="black")
+ax2.set_ylabel("Precipitation [m/s]")
+ax2.set_xlabel("Day of the year")
+ax2.axvline(WSL_onset[0,0], color="C0", label="WS onset 1")
+ax2.axvline(WSL_onset[1,0]*5, color="C1", label="WS onset 2")
+ax2.axvline(WSL_onset[2,0]*5, color="C2", label="WS onset 3")
+
+
+ax2.axvline(DSL_onset[0,0], color="C0",  ls="--", label="DS onset 1")
+ax2.axvline(DSL_onset[1,0]*5, color="C1",  ls="--", label="DS onset 2")
+ax2.axvline(DSL_onset[2,0]*5, color="C2", ls="--", label="DS onset 3")
+ax2.legend(ncol=2)
+plt.tight_layout()
+plt.show()
